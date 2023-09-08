6a28bf8de5f05c7f37fcaa0ffe14677ba8003088
A Potašenkov <a.potasenkov@voco.ee>
2023-09-08 11:45:28 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+const pers = {
+    firstname: 'Saas'
+,    lastname: 'sass'
+}      
+
+console.log(pers)
+console.log(pers.firstname)
+console.log(pers.lastname)
+pers.department = 'IT AK'
+console.log(pers)
